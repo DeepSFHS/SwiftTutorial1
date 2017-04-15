f826bfc9b62d32c048bbee17499c3bd377d3f397
Deepankar Joshi <Deep@deepankars-air.attlocal.net>
2017-04-15 16:55:44 -0700

    initialcommit

---
+++ Filterer/ViewController.swift
@@ -0,0 +1,175 @@
+//
+//  ViewController.swift
+//  Filterer
+//
+//  Created by Jack on 2015-09-22.
+//  Copyright © 2015 UofT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var filteredImage: UIImage?
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var secondaryMenu: UIView!
+    @IBOutlet weak var Green: UIButton!
+    @IBOutlet var bottomMenu: UIView!
+    
+    @IBOutlet var filterButton: UIButton!
+    
+    
+    
+    @IBAction func onGreen(_ sender: UIButton) {
+        if Green.isSelected {
+            let image = UIImage(named: "scenery")!
+            imageView.image = image
+            Green.isSelected = false
+        } else {
+            Green.isSelected = true
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        secondaryMenu.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        secondaryMenu.translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImage(named: "scenery")!
+        
+        
+        
+        var rgbaImage = RGBAImage(image: image)!
+        
+        
+        
+        let avgColor = 107
+        
+        
+        
+        for y in 0..<rgbaImage.height {
+            
+            for x in 0..<rgbaImage.width {
+                
+                let index = y * rgbaImage.width + x
+                
+                
+                
+                var pixel = rgbaImage.pixels[index]
+                    let redDelta = Int(pixel.red) - avgColor
+                    
+                    
+                    var modifier = 1 + 4 * (Double(y)) / Double(rgbaImage.height)
+                    
+                    if (Int(pixel.red) < avgColor) {
+                        
+                        modifier  = 1
+                        
+                    }
+                    
+                    
+                    
+                    pixel.red = UInt8(max(min(255, Int(round(Double(avgColor) + modifier * Double (redDelta)))), 0))
+                    
+                    rgbaImage.pixels[index] = pixel
+
+    }
+        }
+         filteredImage = rgbaImage.toUIImage()
+        
+    }
+    // MARK: Share
+    @IBAction func onShare(_ sender: AnyObject) {
+        let activityController = UIActivityViewController(activityItems: ["Check out our really cool app", imageView.image!], applicationActivities: nil)
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    // MARK: New Photo
+    @IBAction func onNewPhoto(_ sender: AnyObject) {
+        let actionSheet = UIAlertController(title: "New Photo", message: nil, preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { action in
+            self.showCamera()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Album", style: .default, handler: { action in
+            self.showAlbum()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func showCamera() {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .camera
+        
+        present(cameraPicker, animated: true, completion: nil)
+    }
+    
+    func showAlbum() {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .photoLibrary
+        
+        present(cameraPicker, animated: true, completion: nil)
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: Filter Menu
+    @IBAction func onFilter(_ sender: UIButton) {
+        if (sender.isSelected) {
+            hideSecondaryMenu()
+            sender.isSelected = false
+        } else {
+            showSecondaryMenu()
+            sender.isSelected = true
+        }
+    }
+    
+    func showSecondaryMenu() {
+        view.addSubview(secondaryMenu)
+        
+        let bottomConstraint = secondaryMenu.bottomAnchor.constraint(equalTo: bottomMenu.topAnchor)
+        let leftConstraint = secondaryMenu.leftAnchor.constraint(equalTo: view.leftAnchor)
+        let rightConstraint = secondaryMenu.rightAnchor.constraint(equalTo: view.rightAnchor)
+        
+        let heightConstraint = secondaryMenu.heightAnchor.constraint(equalToConstant: 44)
+        
+        NSLayoutConstraint.activate([bottomConstraint, leftConstraint, rightConstraint, heightConstraint])
+        
+        view.layoutIfNeeded()
+        
+        self.secondaryMenu.alpha = 0
+        UIView.animate(withDuration: 0.4, animations: {
+            self.secondaryMenu.alpha = 1.0
+        }) 
+    }
+    
+    
+    func hideSecondaryMenu() {
+        UIView.animate(withDuration: 0.4, animations: {
+            self.secondaryMenu.alpha = 0
+            }, completion: { completed in
+                if completed == true {
+                    self.secondaryMenu.removeFromSuperview()
+                }
+        }) 
+    }
+
+}
+
